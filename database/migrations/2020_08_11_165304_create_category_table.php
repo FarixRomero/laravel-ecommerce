ecf342488239d7398d70900bb99f6e07596bc6ae
FarixRomeroCode <a20193387@pucp.edu.pe>
2020-08-27 11:05:41 -0500

    fixing

--- database/migrations/2020_08_11_165304_create_category_table.php
+++ database/migrations/2020_08_11_165304_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('category');
     }
 }
